e94b52c2778db81fd5347ff35c24009d922aea4a
aniruddha <aniruddha.kudalkar@gmail.com>
2017-11-14 09:26:54 +0530

    observables: what, hot and cold observables

--- src/app/observables/observables.component.ts
+++ src/app/observables/observables.component.ts
@@ -1,21 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+// tslint:disable-next-line:import-blacklist
 import { Observable, Subscription } from 'rxjs/Rx';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-observables',
   templateUrl: './observables.component.html',
   styleUrls: ['./observables.component.css']
 })
-export class ObservablesComponent implements OnInit {
+export class ObservablesComponent implements OnInit, OnDestroy {
 
   cntr = 1;
-  constructor() { }
+  wsCh: any;
 
+  subInt: Subscription;
+  subMy: Subscription;
+
+  constructor() {
+    this.wsCh = {};
+  }
   ngOnInit() {
-    Observable.interval(1500).subscribe(
+    this.subInt = Observable.interval(1500)
+      // .catch(this.errHndlr)
+      .subscribe(
       ev => this.cntr += 1,
       err => console.log(err),
       () => console.log(`Completed`)
-    );
+      );
+
+    this.subMy = this.myObservable()
+      // .catch(this.errHndlr)
+      .subscribe();
+  }
+
+  errHndlr(err: any) {
+    return Observable.throw({});
+  }
+  myObservable(): Observable<any> {
+    return Observable.create(sub => {
+      // here notification will arrive, by using websockets
+
+      for (let i = 0; i < 10; i++) {
+
+        if (i > 10) { //any condition
+          sub.error({});
+          Observable.throw({ msg: 'Bad Condition' });
+        }
+        sub.next('new unread mail');
+      }
+      sub.complete();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subInt.unsubscribe();
+    this.subMy.unsubscribe();
   }
 }
